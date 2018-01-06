008580fa4948de107764519a85e3945c37ea74da
van <van@hovercloud.com>
2018-01-06 09:37:25 -0800

    implementation of a one pass solution for gas station problem (there is still a corner case not working though)

--- GasStation.java
+++ GasStation.java
@@ -19,10 +19,51 @@ Brute force algorithm:
 - as soon as we find a starting station that works, return it,
 - if we get through trying them all, and there are none that work, return -1.
 
+While I was testing this, I noticed that a solution is only possible if you have
+as much gas in the system and there are costs to go around the circle.  And even
+then it typically works best if you start at the station with the most gas,
+and pick up all the gas at every station on the way. You've got to think of the
+cost of movement too, and take that into account.  So actually, the best station
+to start at is the one where there is the biggest surplus of gas when you subtract
+how much it will cost to get to the next station.
+
+A more efficient algorithm that can be done in a single pass through the data
+would be to find the maximumally best place to start and get the highest
+surplus of gas left over, which will then help you make it through all subsequent
+stations.
+
+Addendum: this still doesn't work if the station you need to start at is the last
+one on the list (see case12 in the tests.  So I need to go looking in the
+discussion for a hint and I'll come back to this.
 */
 public class GasStation {
 
     public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalGas = 0;
+        int totalCost = 0;
+        int maxSurplusAmount = 0;
+        int maxSurplusStation = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+            totalGas += gas[i];
+            totalCost += cost[i];
+
+            int surplusHere = gas[i] - cost[i];
+            if (surplusHere > maxSurplusAmount) {
+                maxSurplusAmount = surplusHere;
+                maxSurplusStation = i;
+            }
+        }
+
+        if (totalGas < totalCost) {
+            return -1;
+        }
+        else {
+            return maxSurplusStation;
+        }
+    }
+
+    public int canCompleteCircuitBruteForce(int[] gas, int[] cost) {
         int numStations = gas.length;
         for (int startingStation = 0; startingStation < numStations; startingStation++) {
             int gasInTank = 0;
@@ -44,18 +85,30 @@ public class GasStation {
     }
 
     public static void main(String[] args) {
-        int N = 5;
         int[] cost = { 3, 3, 3, 3, 3 };
-
         int[] gas1 = { 1, 2, 8, 2, 1 };
         int[] gas2 = { 1, 2, 9, 2, 1 };
         int[] gas3 = { 3, 3, 3, 3, 2 };
         int[] gas4 = { 2, 3, 3, 3, 4 };
 
+        int[] gas11 = { 1, 2, 3, 4, 5 };
+        int[] cost11 = { 3, 4, 5, 1, 2 };
+        int[] gas12 = { 6, 1, 2, 3, 3 };
+        int[] cost12 = { 2, 3, 4, 5, 1 };
+
         GasStation gs = new GasStation();
-        System.out.println("starting gas station is " + gs.canCompleteCircuit(gas1, cost) + " (should be -1)");
-        System.out.println("starting gas station is " + gs.canCompleteCircuit(gas2, cost) + " (should be 2)");
-        System.out.println("starting gas station is " + gs.canCompleteCircuit(gas3, cost) + " (should be -1)");
-        System.out.println("starting gas station is " + gs.canCompleteCircuit(gas4, cost) + " (should be 1)");
+        System.out.println("starting gas station is " + gs.canCompleteCircuit(gas1, cost) + " (should be -1)" +
+                " brute force result is " + gs.canCompleteCircuitBruteForce(gas1, cost));
+        System.out.println("starting gas station is " + gs.canCompleteCircuit(gas2, cost) + " (should be 2)" +
+                " brute force result is " + gs.canCompleteCircuitBruteForce(gas2, cost));
+        System.out.println("starting gas station is " + gs.canCompleteCircuit(gas3, cost) + " (should be -1)" +
+                " brute force result is " + gs.canCompleteCircuitBruteForce(gas3, cost));
+        System.out.println("starting gas station is " + gs.canCompleteCircuit(gas4, cost) + " (should be 1)" +
+                " brute force result is " + gs.canCompleteCircuitBruteForce(gas4, cost));
+
+        System.out.println("starting gas station is " + gs.canCompleteCircuit(gas11, cost11) + " (should be 3)" +
+                " brute force result is " + gs.canCompleteCircuitBruteForce(gas11, cost11));
+        System.out.println("starting gas station is " + gs.canCompleteCircuit(gas12, cost12) + " (should be 4)" +
+                " brute force result is " + gs.canCompleteCircuitBruteForce(gas12, cost12));
     }
 }
