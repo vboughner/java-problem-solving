fe5e62dc4038de5edaf9a9cf88208a2c93348b81
Van Boughner <van.boughner@ntti3.io>
2018-02-12 13:47:44 -0800

    fixed bugs in solution to the DecodeWays problem

--- DecodeWays.java
+++ DecodeWays.java
@@ -21,6 +21,12 @@ The number of ways decoding "12" is 2.
 
 public class DecodeWays {
     private int numDecodingsHelper(Map<String,Integer> numWays, String s, int index) {
+        if (s.length() - index == 0) {
+            return 1;
+        }
+        if (s.charAt(index) == '0') {
+            return 0;
+        }
         if (s.length() - index <= 1) {
             return 1;
         }
@@ -32,7 +38,16 @@ public class DecodeWays {
 
         int retval;
         int pairValue = Integer.valueOf(s.substring(index, index + 2));
-        if (pairValue > 26) {
+        if (pairValue == 0) {
+            return 0;
+        }
+        else if (pairValue > 26) {
+            retval = numDecodingsHelper(numWays, s, index + 1);
+        }
+        else if (pairValue == 10 || pairValue == 20) {
+            retval = numDecodingsHelper(numWays, s, index + 2);
+        }
+        else if (s.length() - index > 2 && s.charAt(index + 2) == '0') {
             retval = numDecodingsHelper(numWays, s, index + 1);
         }
         else {
@@ -57,19 +72,25 @@ public class DecodeWays {
     static public void main(String[] args) {
         DecodeWays dw = new DecodeWays();
         String case0 = "";
-        String case1 = "1";
-        String case2 = "3";
-        String case3 = "12";
-        String case4 = "1221";
-        String case5 = "1261";
-        String case6 = "1271";
+        String case1 = "0";
+        String case2 = "1";
+        String case3 = "3";
+        String case4 = "10";
+        String case5 = "12";
+        String case6 = "1221";
+        String case7 = "1261";
+        String case8 = "1271";
+        String case9 = "1101";
 
-        System.out.println("'" + case0 + "' has " + dw.numDecodings(case0) + " decodings");
-        System.out.println("'" + case1 + "' has " + dw.numDecodings(case1) + " decoding");
-        System.out.println("'" + case2 + "' has " + dw.numDecodings(case2) + " decoding");
-        System.out.println("'" + case3 + "' has " + dw.numDecodings(case3) + " decodings");
-        System.out.println("'" + case4 + "' has " + dw.numDecodings(case4) + " decodings");
-        System.out.println("'" + case5 + "' has " + dw.numDecodings(case5) + " decodings");
-        System.out.println("'" + case6 + "' has " + dw.numDecodings(case6) + " decodings");
+        System.out.println("'" + case0 + "' has " + dw.numDecodings(case0) + " decodings (0)");
+        System.out.println("'" + case1 + "' has " + dw.numDecodings(case1) + " decodings (0)");
+        System.out.println("'" + case2 + "' has " + dw.numDecodings(case2) + " decoding (1)");
+        System.out.println("'" + case3 + "' has " + dw.numDecodings(case3) + " decoding (1)");
+        System.out.println("'" + case4 + "' has " + dw.numDecodings(case4) + " decoding (1)");
+        System.out.println("'" + case5 + "' has " + dw.numDecodings(case5) + " decodings (2)");
+        System.out.println("'" + case6 + "' has " + dw.numDecodings(case6) + " decodings (5)");
+        System.out.println("'" + case7 + "' has " + dw.numDecodings(case7) + " decodings (3)");
+        System.out.println("'" + case8 + "' has " + dw.numDecodings(case8) + " decodings (2)");
+        System.out.println("'" + case9 + "' has " + dw.numDecodings(case9) + " decoding (1)");
     }
 }
